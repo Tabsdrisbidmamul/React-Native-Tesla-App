8fdb3d06c8fe55feba15544ba9fe699f045f570a
Idris Khan <ricepoo123@gmail.com>
2021-04-23 22:43:50 +0100

    intial commit

---
+++ components/StyledButton/StyledButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./styles";
+
+const StyledButton = (props) => {
+  return (
+    <View>
+      <Text>Some Text</Text>
+    </View>
+  );
+};
+
+export default StyledButton;
