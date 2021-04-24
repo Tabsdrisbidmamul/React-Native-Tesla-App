781b719be46ca3b63ce09df234a7110606232c43
Idris Khan <ricepoo123@gmail.com>
2021-04-24 14:14:03 +0100

    Header done

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { CarList } from './components';
+import { CarList, Header } from './components';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <CarList />
 
       <StatusBar style="auto" />
--- components/index.js
+++ components/index.js
@@ -1,1 +1,2 @@
 export { default as CarList } from './CarList/CarList';
+export { default as Header } from './Header/Header';
